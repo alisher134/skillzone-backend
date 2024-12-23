02c3975b1a51ef80c6065cb7544ef35f9ebd7180
alisher134 <alisherrakhmanov134@gmail.com>
2024-12-23 20:56:53 +0500

    chore: add config for prettier, eslint and husky

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 import { getCorsConfig } from './config/cors.config';
 
