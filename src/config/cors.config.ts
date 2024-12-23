da1886cf18b70d53ecaec572253292079ba04deb
alisher134 <alisherrakhmanov134@gmail.com>
2024-12-23 20:33:01 +0500

    chore: add base bootstrap configuration and cors config

---
+++ src/config/cors.config.ts
@@ -0,0 +1,9 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
+
+export const getCorsConfig = (configService: ConfigService): CorsOptions => ({
+  origin: configService.getOrThrow<string>('ALLOWED_ORIGIN'),
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  exposedHeaders: ['set-cookie'],
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { getCorsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+
+  const cookieSecret = config.getOrThrow<string>('COOKIE_SECRET');
+  app.use(cookieParser(cookieSecret));
+
+  app.setGlobalPrefix('v1/api');
+  app.enableCors(getCorsConfig(config));
+
+  const port = config.getOrThrow<number>('PORT');
+  await app.listen(port);
 }
 bootstrap();
